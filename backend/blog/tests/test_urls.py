531c27720c77015c3dbcde74903d6900e652baca
Kazama Kotaro <kazamakotaro@sakailab-mac-02.local>
2021-08-31 17:09:04 +0900

    indexページへのアクセステストを削除

--- backend/blog/tests/test_urls.py
+++ backend/blog/tests/test_urls.py
@@ -3,13 +3,7 @@ from django.urls import reverse, resolve
 from ..views import IndexView, PostListView
 
 class TestUrls(TestCase):
-
-  """index ページへのURLでアクセスする時のリダイレクトをテスト"""
-  def test_post_index_url(self):
-    view = resolve('/blog/')
-    self.assertEqual(view.func.view_class, IndexView)
-
-  """Post 一覧ページへのリダイレクトをテスト"""
+  #Post 一覧ページへのリダイレクトをテスト
   def test_post_list_url(self):
     view = resolve('/blog/post_list')
     self.assertEqual(view.func.view_class, PostListView)
