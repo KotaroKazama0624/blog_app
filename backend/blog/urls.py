e4ab38f5bc45116dc05c1830e44f7627be2e80fe
Kazama Kotaro <kazamakotaro@sakailab-mac-02.local>
2021-08-31 14:01:49 +0900

    詳細・編集・削除のルーティングを追加しました。

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
-from .views import PostListView, IndexView, PostCreateView
+from .views import PostListView, IndexView, PostCreateView, PostDetailView, PostUpdateView, PostDeleteView
 
 app_name = 'blog'
 
 urlpatterns = [
-   path('', IndexView.as_view(), name='index'),
-   path('post_list', PostListView.as_view(), name='post_list'),
-   path('post_create', PostCreateView.as_view(), name='post_create'),
+    path('', IndexView.as_view(), name='index'),
+    path('post_list', PostListView.as_view(), name='post_list'),
+    path('post_create', PostCreateView.as_view(), name='post_create'),
+    path('post_detail/<int:pk>/', PostDetailView.as_view(), name='post_detail'), 
+    path('post_update/<int:pk>/', PostUpdateView.as_view(), name='post_update'), 
+    path('post_delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'), 
 ]
