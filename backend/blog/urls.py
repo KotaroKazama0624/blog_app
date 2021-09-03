26156dba82672aef8ac0c6081dd0fecad7d912f4
Kazama Kotaro <kazamakotaro@sakailab-mac-02.local>
2021-09-03 13:22:10 +0900

    loginのルーチンを追加

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -4,9 +4,10 @@ from .views import PostListView, PostCreateView, PostDetailView, PostUpdateView,
 app_name = 'blog'
 
 urlpatterns = [
-    path('usercreate', usercreatefunc),
-    path('post_list', PostListView.as_view(), name='post_list'),
-    path('post_create', PostCreateView.as_view(), name='post_create'),
+    path('usercreate/', usercreatefunc),
+    path('login/', loginfunc),
+    path('post_list/', PostListView.as_view(), name='post_list'),
+    path('post_create/', PostCreateView.as_view(), name='post_create'),
     path('post_detail/<int:pk>/', PostDetailView.as_view(), name='post_detail'), 
     path('post_update/<int:pk>/', PostUpdateView.as_view(), name='post_update'), 
     path('post_delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'), 
